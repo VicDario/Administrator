7e6d42d3867344ab22d0cef54d7236459a1cd5f3
Yurb19 <45300273+Yurb19@users.noreply.github.com>
2020-01-28 12:29:07 -0400

    deleting node_modules & working with .env

---
+++ src/Administrator.js
@@ -0,0 +1,60 @@
+// Requiriendo la API de discord.js
+const Discord = require("discord.js");
+const client = new Discord.Client(); 
+const config = require("./config.json"); // Configuracion del Bot y public-key
+var prefix = config.prefix; // bot-prefix
+
+
+// Funciones (Comandos del bot)
+
+function kick(args, command, message){ // Kicking an user
+
+  let user = message.mentions.users.first();
+  let razon = args.slice(1).join(' ');
+  
+  if (message.mentions.users.size < 1) return message.reply('Debe mencionar a alguien.').catch(console.error);
+  if (!razon) return message.channel.send('Escriba una razón, `-kick @username [razón]`');
+  if (!message.guild.member(user).kickable) return message.reply('No puedo patear al usuario mencionado.');
+   
+  message.guild.member(user).kick(razon);
+  message.channel.send(`**${user.username}**, fue pateado del servidor, razón: ${razon}.`);
+
+}
+
+function ping(args, command, message){ // ping test
+
+  let ping = Math.floor(message.client.ping);
+  
+  message.channel.send(":ping_pong: Pong!")
+   .then(m => {
+
+     m.edit(`:incoming_envelope: Ping Mensajes: \`${Math.floor(m.createdTimestamp - Date.now())} ms\`\n:satellite_orbital: Ping DiscordAPI: \`${ping} ms\``);
+   });
+
+}
+
+// terminan las funciones
+
+
+// Iniciando el bot
+client.on("ready", () => {
+   console.log("Estoy listo!");
+});
+
+
+// implementando los comandos
+client.on("message", (message) => {
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+ if (!message.content.startsWith(config.prefix)) return;
+ if (message.author.bot) return;
+
+ if(command === 'kick') kick(args, command, message); 
+
+  if (command === 'ping') ping(args, command, message);
+
+});
+client.login(config.token);     
+       
