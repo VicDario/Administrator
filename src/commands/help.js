1dc7cd29d3c129ad5b42b741424ca0a68dba388a
Yurb19 <45300273+Yurb19@users.noreply.github.com>
2020-02-05 11:43:03 -0400

    improving help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,3 +1,28 @@
 exports.run = (client, message, args) =>{
-    message.channel.send('Puedo realizar las siguientes acciones:\n-kick @User Razon\n-clear @user\n-ping');
+
+    // Requires 
+    const { RichEmbed } = require('discord.js'); // Requiring richembed class to create embed messages
+    const fs = require('fs');
+
+    // Finding commands
+    const commands = fs.readdirSync('./src/commands');
+
+    // Creating embed message
+    const embed = new RichEmbed() 
+    .setTitle('Lista de Comandos') // set the embed message title
+    .setColor(0x726eef) // set embed color
+    .setDescription(content(commands)); // embed content 
+
+    message.channel.send(embed);
+    //-kick @User -motivo \n-clear @User\n-ping
+}
+
+function content(commands){
+    let str = '';
+    for(item of commands){
+        let new_item = item.split('.').shift();
+        str += ('-' + new_item + '\n');
+    }
+
+    return str;
 }
