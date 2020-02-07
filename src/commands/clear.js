a728261b87c1d29ba26ccb4ba8c58fa42986c23a
Yurb19 <45300273+Yurb19@users.noreply.github.com>
2020-02-07 13:28:27 -0400

    clear all messages command

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -1,21 +1,22 @@
 exports.run = (client, message, args) => {
 
     const user = message.mentions.users.first();
-
+    
     if(user){
-        const member = message.guild.member(user);
-
-        if(member){
             
-            message.channel.fetchMessages()
+            message.channel.fetchMessages({limit: 100})
             .then(messages => `${messages.filter(m => {
-               if (m.author.id === member.id){
+               if (m.author.id === user.id){
                    m.delete().catch(console.error)
                }
             
             })}`)
             .catch(console.error);
 
-        }
+    }else {
+        message.channel.fetchMessages({limit: 100})
+        .then(messages => messages.filter(m => {
+                m.delete().catch(console.error)
+         })).catch(console.error);
     }
 }
--- src/commands/set_role.js
+++ src/commands/set_role.js
@@ -1,3 +1,21 @@
-exports.run = (client, message, args) => {
-  message.reply('No puedo hacer nada we, toy chiquito :(');
+exports.run = async(client, message, args) => {
+
+  const collaborator = process.env.COLLABORATOR;
+  const admin = process.env.ADMIN;
+  
+  const { RichEmbed } = require('discord.js');
+  const embed = new RichEmbed();
+  const user = message.mentions.users.first();
+  
+  try{
+      if(user){
+        console.log(message.guild.roles.array())
+      }else{
+        message.reply('debes mencionar un usuario')
+      }
+  }catch(e){
+      console.log(e);
+  }
+
+  
 }
