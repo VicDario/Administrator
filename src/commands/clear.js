3127efef3c3ce4c1d11031f21c47f7348fdfe682
Yurb19 <45300273+Yurb19@users.noreply.github.com>
2020-02-07 14:05:43 -0400

    clear all messages command improved

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -3,30 +3,21 @@ exports.run = (client, message, args) => {
     const user = message.mentions.users.first();
     
     if(user){
-
-    const member = message.guild.member(user);
-
-    if(member){
             
             message.channel.fetchMessages({limit: 100})
-            .then(messages => `${messages.filter(m => {
+            .then(messages => messages.filter(m => {
                if (m.author.id === user.id){
-                   m.delete().catch(console.error)
+                   m.delete().catch(console.error);
                }
             
-            })}`)
+            }))
             .catch(console.error);
 
     }else {
         message.channel.fetchMessages({limit: 100})
         .then(messages => messages.filter(m => {
-                m.delete().catch(console.error)
+                m.delete().catch(console.error);
          })).catch(console.error);
-    }else{
-        for(int x=0;x>20;x++){
-            message.channel.fetchMessages().delete().catch(console.error)
-        }
-        
     }
 }
-}
+
