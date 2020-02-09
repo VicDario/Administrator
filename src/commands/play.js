c907feb2501cea9ef1cae93bbdfba48e72d6c469
Yurb19 <45300273+Yurb19@users.noreply.github.com>
2020-02-08 23:44:52 -0400

    play command improved

--- src/commands/play.js
+++ src/commands/play.js
@@ -1,10 +1,5 @@
 exports.run = async (client, message, args) => {
 
-    // const { spawn } = require('child_process');
-
-    const ffmpegPath = require('ffmpeg-static');
-    // spawn(ffmpegPath, ['-i', 'video.mkv', 'video.mp4']);
-
     //Requiring Google API
     const {google} = require('googleapis');
     const youtube = google.youtube({
@@ -12,26 +7,57 @@ exports.run = async (client, message, args) => {
         auth: process.env.APIKEY
     });
 
+    // Requiring ytdl
+    const ytdl = require('ytdl-core');
+
+
     if(!message.guild) return;
 
-    if(message.member.voiceChannel){
-        message.member.voiceChannel.join();
-        // console.log(process.env.PATH)
-    }else {
-        message.reply('conectate a un canal de voz');
+    if(args.length == 0) {
+        message.reply('ingresa una cancion.');
+        return;
+    }else{
+
+        let query = '';
+        for(param of args){
+            query += param + ' ';
+        }
+
+        if(message.member.voiceChannel){
+            
+            try{
+                const conn = await message.member.voiceChannel.join();
+                const video =  await youtube.search.list({
+                    part: 'id,snippet',
+                    q: query,
+                    type: 'video',
+                    maxResults: 1
+                });
+
+                let url = `https://www.youtube.com/watch?v=${video.data.items[0].id.videoId}`;
+
+                const stream = ytdl(url, { filter: 'audioonly' });
+                const dispatcher = conn.playStream(stream);
+
+                dispatcher.on('end', () => voiceChannel.leave());
+
+                console.log(video.data.items[0].snippet.thumbnails);
+            }catch(e){
+                message.send('se produjo un error al tratar de reproducir la cancion');
+                console.log(e);
+            }
+            
+            
+        }else {
+            message.reply('conectate a un canal de voz');
+        }
+    
+        
+
+
     }
 
-    // try{
-    //     const video =  await youtube.search.list({
-    //         part: 'id,snippet',
-    //         q: 'goosebumps',
-    //         type: 'video',
-    //         maxResults: 1
-    //     });
-    //     console.log(video.data.items[0].snippet.thumbnails);
-    // }catch(e){
-    //     console.log(e);
-    // }
+    
 
 
 }
