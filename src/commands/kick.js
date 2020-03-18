ac1ab22a026721de5e00cf1d7270c6791d008398
Yurb19 <45300273+Yurb19@users.noreply.github.com>
2020-03-18 11:06:02 -0400

    custom message when kicking rafael

--- src/commands/kick.js
+++ src/commands/kick.js
@@ -1,22 +1,29 @@
 exports.run = (client, message, args) => { // Kicking an user
 
+  // Administrator and Collaborators ID's
   const collaborator = process.env.COLLABORATOR;
   const admin = process.env.ADMIN;
 
+  // This is for a friend request xdd - Will generate a custom message if you try to kick the specific user
+  const tkid = process.env.TK_ID;
 
   let user = message.mentions.users.first();
   let razon = args.slice(1).join(' ');
 
   if(message.member.roles.has(admin) || message.member.roles.has(collaborator)){
     
-    // // if (message.mentions.users.size < 1) return message.reply('Debe mencionar a alguien.').catch(console.error);
-    // if (!razon) return message.channel.send('Escriba una razón, `-kick @username [razón]`');
-    // // if (!message.guild.member(user).kickable) return message.reply('No puedo patear al usuario mencionado.');
-    //  
-    // message.guild.member(user).kick(razon);
-    // message.channel.send(`**${user.username}**, fue pateado del servidor, razón: ${razon}.`);
+    if (message.mentions.users.size < 1) return message.reply('Debe mencionar a alguien.').catch(console.error);
+    if (!razon) return message.channel.send('Escriba una razón, `-kick @username [razón]`');
+    if (!message.guild.member(user).kickable) return message.reply('No puedo patear al usuario mencionado.');
+     
+    message.guild.member(user).kick(razon);
+    message.channel.send(`**${user.username}**, fue pateado del servidor, razón: ${razon}.`);
     console.log(user);
   }else {
+    if(user.id == tkid){
+      message.reply('coño si quisiera, pero muy rata vale');
+      return;
+    }
     message.reply('ysi noquiero q');
   }
   
