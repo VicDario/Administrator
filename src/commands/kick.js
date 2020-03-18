2767f573cff3982bd9c43c7d7f9c02581d8285ad
Yurb19 <45300273+Yurb19@users.noreply.github.com>
2020-03-18 10:53:33 -0400

    testing

--- src/commands/kick.js
+++ src/commands/kick.js
@@ -3,16 +3,19 @@ exports.run = (client, message, args) => { // Kicking an user
   const collaborator = process.env.COLLABORATOR;
   const admin = process.env.ADMIN;
 
+
+  let user = message.mentions.users.first();
+  let razon = args.slice(1).join(' ');
+
   if(message.member.roles.has(admin) || message.member.roles.has(collaborator)){
-    let user = message.mentions.users.first();
-    let razon = args.slice(1).join(' ');
     
-    if (message.mentions.users.size < 1) return message.reply('Debe mencionar a alguien.').catch(console.error);
-    if (!razon) return message.channel.send('Escriba una razón, `-kick @username [razón]`');
-    if (!message.guild.member(user).kickable) return message.reply('No puedo patear al usuario mencionado.');
-     
-    message.guild.member(user).kick(razon);
-    message.channel.send(`**${user.username}**, fue pateado del servidor, razón: ${razon}.`);
+    // // if (message.mentions.users.size < 1) return message.reply('Debe mencionar a alguien.').catch(console.error);
+    // if (!razon) return message.channel.send('Escriba una razón, `-kick @username [razón]`');
+    // // if (!message.guild.member(user).kickable) return message.reply('No puedo patear al usuario mencionado.');
+    //  
+    // message.guild.member(user).kick(razon);
+    // message.channel.send(`**${user.username}**, fue pateado del servidor, razón: ${razon}.`);
+    console.log(user);
   }else {
     message.reply('ysi noquiero q');
   }
