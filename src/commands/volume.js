202d33b044e466d462f28ad8fa66fa61fb313691
VicDario <26585356+VicDario@users.noreply.github.com>
2020-03-23 19:15:34 -0300

    Fix volume

--- src/commands/volume.js
+++ src/commands/volume.js
@@ -3,8 +3,8 @@ exports.run = async (client, message, args) => {
     const conn = await message.member.voiceChannel.join();
 
     if(message.member.voiceChannel){
-        conn.dispatcher.setVolume(Number.parseFloat(message));
-        message.channel.send("A ber" + message);
+        
+        conn.dispatcher.setVolume(Number.parseFloat(slice(message.length, message.length - 2 )));
     }
 
 }
