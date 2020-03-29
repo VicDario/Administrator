5551005776116fa75cfe4915170eb212cdd52f00
VicDario <26585356+VicDario@users.noreply.github.com>
2020-03-29 15:01:03 -0300

    "Test" message removed

--- src/commands/pause.js
+++ src/commands/pause.js
@@ -7,8 +7,6 @@ exports.run = async (client, message, args) => {
 
             if(conn.speaking){
                 conn.dispatcher.pause();
-                //conn.disconnect();
-                message.reply('este comando esta en fase alpha, puede que no responda como deberia y marico el que lo lea');
             }else {
                 message.channel.send('No se esta reproduciendo nada.');
             }
