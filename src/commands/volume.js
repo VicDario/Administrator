1a252baf81901a1d82f48dc8a8665af891d1bc12
VicDario <26585356+VicDario@users.noreply.github.com>
2020-03-28 22:54:31 -0300

    Fix volume command

--- src/commands/volume.js
+++ src/commands/volume.js
@@ -1,12 +1,12 @@
 exports.run = async (client, message, args) => {
 
     const conn = await message.member.voiceChannel.join();
-
-    if(message.member.voiceChannel){
+    if(message.member.voiceChannel && Number.parseFloat(message.content.slice(7)) > 0 && Number.parseFloat(message.content.slice(7)) < 11){
         
-        //conn.dispatcher.setVolume(Number.parseFloat(slice(message.length, message.length - 2 )));
-        message.channel.send(slice(message.length, message.length - 2 ));
+       conn.dispatcher.setVolume(Number.parseFloat(message.content.slice(7)) / 10);
 
+    }else{
+        message.reply("Ocurrio un problema");
     }
 
 }
