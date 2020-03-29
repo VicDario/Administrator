1b036d40fc3c30ffc289e5bc450c2778b1bd9d28
VicDario <26585356+VicDario@users.noreply.github.com>
2020-03-29 13:18:40 -0300

    Changes in volume

--- src/commands/volume.js
+++ src/commands/volume.js
@@ -1,9 +1,9 @@
 exports.run = async (client, message, args) => {
 
     const conn = await message.member.voiceChannel.join();
-    if(message.member.voiceChannel && Number.parseFloat(message.content.slice(7)) > 0 && Number.parseFloat(message.content.slice(7)) < 11){
+    if(message.member.voiceChannel && Number.parseFloat(message.content.slice(7)) > 0 && Number.parseFloat(message.content.slice(7)) < 101){
         
-       conn.dispatcher.setVolume(Number.parseFloat(message.content.slice(7)) / 10);
+       conn.dispatcher.setVolume(Number.parseFloat(message.content.slice(7)) / 100);
 
     }else{
         message.reply("Ocurrio un problema");
