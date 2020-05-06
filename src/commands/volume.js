b5d78ca0c1cc440c8fc9be76a8e324531fd24077
Yurb19 <yavurb@gmail.com>
2020-05-06 18:35:38 -0400

    volume command fixed

--- src/commands/volume.js
+++ src/commands/volume.js
@@ -1,29 +1,29 @@
 exports.run = async (client, message, args) => {
+	try {
+	const conn = await message.member.voice.channel.join();
+	const dispatcher = conn.dispatcher;
+	const volume = parseFloat(args.join(""));
+
 	if (!message.member.voice.channel) {
 		message.reply("debes estar conectado a un canal de voz");
+		conn.disconnect();
+		return;
+	}
+	
+	if(!conn.speaking){
+		message.reply('no estas escuchando nada papito mi rey');
+		conn.disconnect();
 		return;
 	}
 
-    const conn = await message.member.voiceChannel.join();
-	const dispatcher = conn.dispatcher;
-
-	if(!conn.speaking){
-        message.reply('no estas escuchando nada papito mi rey');
-	}
-
-	const volume = parseFloat(args.join(""));
-	console.log(volume);
-
-	message.channel.send("xdd");
-
 	if (isNaN(volume) || volume < 0 || volume > 100) {
 		message.reply("debes ingresar un numero de 1 a 100");
+		return;
 	}
 
-    conn.dispatcher.setVolume(volume / 100);
-	// try {
-	// } catch (error) {
-	// 	console.log(error);
-	// 	message.channel.send("ocurrio un erro");
-	// }
+    dispatcher.setVolume(volume / 100);
+	} catch (error) {
+		console.log(error);
+		message.channel.send("ocurrio un error");
+	}
 };
