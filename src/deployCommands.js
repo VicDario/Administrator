5e7656e4d991d2addc9b5900b21cd2017fe78d49
Victor Fernandez <vfernandez@dcanje.com>
2022-02-04 19:25:43 -0300

    [Add] DeployCommands for new Discord API

---
+++ src/deployCommands.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const {REST} = require('@discordjs/rest');
+const {Routes} = require('discord-api-types/v9');
+const path = require('path');
+require('dotenv').config();
+
+const clientId = process.env.CLIENT_ID;
+const token = process.env.TOKEN;
+const guildId = process.env.GUILD_ID;
+
+
+const commands = [];
+const commandFiles = fs.readdirSync(
+    path.resolve(path.basename(__dirname),
+        'commands'),
+)
+    .filter((file) =>
+      file.endsWith('.js'),
+    );
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({version: '9'}).setToken(token);
+
+(async () => {
+  try {
+    await rest.put(
+        Routes.applicationCommand(clientId, guildId),
+        {body: commands},
+    );
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
