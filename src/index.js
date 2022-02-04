2867f6c9a54ea24413eb28ec799b7b86af072597
Victor Fernandez <vfernandez@dcanje.com>
2022-02-04 19:26:55 -0300

    [Update] To Discord.js 13

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 // Requiring Discord.js && other dependencies
-const {Client, Intents} = require('discord.js');
+const {Client, Intents, Collection} = require('discord.js');
+const fs = require('fs');
+const path = require('path');
 require('dotenv').config(); // requiring .env file
 
 // Defining a new Bot
@@ -7,35 +9,47 @@ const client = new Client({intents: [Intents.FLAGS.GUILDS]});
 
 // Saving environment variables
 const token = process.env.TOKEN; // Discord token
-const prefix = process.env.PREFIX; // Bot prefix
+// const prefix = process.env.PREFIX; // Bot prefix
 
 // Initializating
 client.on('ready', () => {
   console.log(`I'm ready as: ${client.user.tag}`);
 });
 
+// Load commands
+client.commands = new Collection();
+const commandFiles = fs.readdirSync(
+    path.resolve(path.basename(__dirname),
+        'commands'),
+)
+    .filter((file) =>
+      file.endsWith('ping.js'),
+    );
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command);
+}
 
 // Listening interactions
 client.on('interactionCreate', async (interaction) => {
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-
-  if (!message.content.startsWith(prefix)) return;
-  if (message.author.bot) return;
-
+  console.log(interaction);
+  if (!interaction.isCommand()) return;
+  const command = client.commands.get(interaction.commandName);
+  if (!command) return;
   // Command Handler
   try {
-    // Cleanign require cache
-    delete require.cache[require.resolve(`./commands/${command}.js`)];
-
-    const rqrCommand = await require(`./commands/${command}.js`);
-    rqrCommand.run(client, message, args);
-  } catch (e) {
-    console.log(e);
-    message.reply('no puedo vieja tonta no bes ke toi chikito');
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply(
+        {
+          content: 'no puedo vieja tonta no bes ke toi chikito',
+          ephemeral: true,
+        },
+    );
   }
 });
-
-
 client.login(token);
-
