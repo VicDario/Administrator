0b90f5e0cdad2799932b3c53ea2b2b1626e190d2
Victor Fernandez <vfernandez@dcanje.com>
2022-02-04 19:27:09 -0300

    [Update] Ping

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,10 +1,25 @@
-exports.run = (client, message, args) => {
+const {SlashCommandBuilder} = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+      .setName('ping')
+      .setDescription('Replies with Pong!'),
+  async execute(interaction) {
+    console.log('aaaaaaaaaaaaaaa');
+    await interaction.reply({
+      content: 'Pong!',
+      ephemeral: true,
+    });
+  },
+};
+
+/* exports.run = (client, message, args) => {
   const ping = Math.floor(message.client.ping);
 
   message.channel.send(':ping_pong: Pong!')
       .then((m) => {
-        m.edit(`:incoming_envelope: Ping Mensajes: 
+        m.edit(`:incoming_envelope: Ping Mensajes:
         \`${Math.floor(m.createdTimestamp - Date.now())} ms\`\n
         :satellite_orbital: Ping DiscordAPI: \`${ping} ms\``);
       });
-};
+}; */
