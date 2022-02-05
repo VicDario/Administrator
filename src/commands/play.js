460c3c0c58bab3488bf6e58f6679a5ce9f3a8fc5
VicDario <26585356+VicDario@users.noreply.github.com>
2022-02-05 19:51:49 -0300

    [Update] play command

--- src/commands/play.js
+++ src/commands/play.js
@@ -1,27 +1,36 @@
-exports.run = async (client, message, args) => {
-  if (!message.guild) return;
+const {SlashCommandBuilder} = require('@discordjs/builders');
+const {google} = require('googleapis');
+const ytdl = require('ytdl-core');
+const {
+  AudioPlayerStatus,
+  StreamType,
+  createAudioPlayer,
+  createAudioResource,
+  joinVoiceChannel,
+} = require('@discordjs/voice');
+const {MessageEmbed} = require('discord.js');
 
-  // Dependencies
-  const {google} = require('googleapis');
-  const ytdl = require('ytdl-core-discord');
-  const {MessageEmbed} = require('discord.js');
-
-  try {
+module.exports = {
+  data: new SlashCommandBuilder()
+      .setName('play')
+      .setDescription('Plays music from youtube')
+      .addStringOption((option) =>
+        option.setName('name')
+            .setDescription('The name of the song to play')
+            .setRequired(true)),
+  async execute(interaction) {
+    const voiceChannel = interaction.member.voice.channel;
+    const ChannelOptions = {
+      channelId: voiceChannel.id,
+      guildId: voiceChannel.guild.id,
+      adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+    };
+    const connection = joinVoiceChannel(ChannelOptions);
     const youtube = google.youtube({
       version: 'v3',
       auth: process.env.APIKEY,
     });
-
-    const embed = new MessageEmbed();
-    const query = args.join(' ');
-
-    if (query.length == 0) return message.reply('ingresa una cancion');
-
-    if (!message.member.voice.channel) {
-      return message.reply('conectate a un canal de voz');
-    }
-
-    const conn = await message.member.voice.channel.join();
+    const query = interaction.options.getString('name');
     const video = await youtube.search.list({
       part: 'id,snippet',
       q: query,
@@ -29,35 +38,23 @@ exports.run = async (client, message, args) => {
       maxResults: 1,
     });
 
-    // console.log(video.data.items[0]);
-
     const url = `https://www.youtube.com/watch?v=${video.data.items[0].id.videoId}`;
-
-    const stream = await ytdl(url, {filter: 'audioonly'});
-    const dispatcher = conn.play(stream,
-        {
-          quality: 'highestaudio',
-          type: 'opus',
-        },
+    const stream = ytdl(url, {filter: 'audioonly'});
+    const resource = createAudioResource(
+        stream,
+        {inputType: StreamType.Arbitrary},
     );
+    const player = createAudioPlayer();
+    player.play(resource);
+    connection.subscribe(player);
+    player.on(AudioPlayerStatus.Idle, () => connection.destroy());
 
-    embed.setTitle(`Reproduciendo: ${video.data.items[0].snippet.title}`);
-    embed.setThumbnail(video.data.items[0].snippet.thumbnails.high.url);
-    embed.setDescription(video.data.items[0].snippet.description);
-    embed.setColor('RANDOM');
+    const musicEmbed = new MessageEmbed()
+        .setColor('RANDOM')
+        .setTitle(video.data.items[0].snippet.title)
+        .setDescription(video.data.items[0].snippet.description)
+        .setThumbnail(video.data.items[0].snippet.thumbnails.high.url);
 
-    message.channel.send(embed);
-
-    dispatcher.on('speaking', (value) => {
-      // return a 0 if stopped speaking
-      setTimeout(() => {
-        if (!value && !dispatcher.paused) {
-          conn.disconnect();
-        }
-      }, 2000);
-      // delaying the disconnect so the pause command can work properly
-    });
-  } catch (e) {
-    console.log(e);
-  }
+    interaction.reply({embeds: [musicEmbed]});
+  },
 };
