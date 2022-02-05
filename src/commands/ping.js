38b2cbbaa8801ba0b7f7b19f74ea9791ad920dd1
VicDario <26585356+VicDario@users.noreply.github.com>
2022-02-05 01:29:47 -0300

    [Complete] Ping command

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -5,21 +5,17 @@ module.exports = {
       .setName('ping')
       .setDescription('Replies with Pong!'),
   async execute(interaction) {
-    console.log('aaaaaaaaaaaaaaa');
-    await interaction.reply({
-      content: 'Pong!',
-      ephemeral: true,
-    });
+    const message = await interaction.reply(
+        {
+          content: ':ping_pong: Pong!',
+          fetchReply: true,
+        },
+    );
+    const ping = message.createdTimestamp - interaction.createdTimestamp;
+    await message.edit(
+        {
+          content: `:ping_pong: Pong! (Took ${ping}ms)`,
+        },
+    );
   },
 };
-
-/* exports.run = (client, message, args) => {
-  const ping = Math.floor(message.client.ping);
-
-  message.channel.send(':ping_pong: Pong!')
-      .then((m) => {
-        m.edit(`:incoming_envelope: Ping Mensajes:
-        \`${Math.floor(m.createdTimestamp - Date.now())} ms\`\n
-        :satellite_orbital: Ping DiscordAPI: \`${ping} ms\``);
-      });
-}; */
