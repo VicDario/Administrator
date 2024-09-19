d13a1e1ff1e29cd5bc1702c2f5287afa059ed41b
VicDario <26585356+VicDario@users.noreply.github.com>
2024-09-19 14:24:42 -0300

    build!: add typescript and prettier config

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+);
