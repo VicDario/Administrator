de3759b6e91d88db4ae863aa7f290f1198e06126
VicDario <26585356+VicDario@users.noreply.github.com>
2024-09-19 20:44:42 -0300

    build: change prettier config

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,17 @@
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
-export default tseslint.config(
-  eslint.configs.recommended,
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
-);
+  {
+    rules: {
+      'prettier/prettier': ['error', { singleQuote: true }],
+    },
+  },
+];
