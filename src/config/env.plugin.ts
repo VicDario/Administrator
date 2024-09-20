253142efa60e801d2258c28d457667bb1a2a4ed3
VicDario <26585356+VicDario@users.noreply.github.com>
2024-09-19 22:19:50 -0300

    MINOR: delete unused env var

--- src/config/env.plugin.ts
+++ src/config/env.plugin.ts
@@ -5,6 +5,5 @@ export const envs = {
   PROD: env.get('PROD').required().asBool(),
   CLIENT_ID: env.get('CLIENT_ID').required().asString(),
   API_TOKEN: env.get('APP_TOKEN').required().asString(),
-  API_KEY: env.get('API_KEY').required().asString(),
   API_VERSION: env.get('API_VERSION').required().asString(),
 };
