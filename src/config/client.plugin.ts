f371533288928b30cf1a71e5e8eeb90326ec0d40
VicDario <26585356+VicDario@users.noreply.github.com>
2024-09-19 23:03:57 -0300

    feat: add client wrapper for discord js

---
+++ src/config/client.plugin.ts
@@ -0,0 +1,83 @@
+import { Player, PlayerEvent } from 'discord-player';
+import {
+  Client,
+  Collection,
+  CommandInteraction,
+  Events,
+  Interaction,
+} from 'discord.js';
+import { ILogger } from '../interfaces/logger.interface';
+import { envs } from './env.plugin';
+import { promises as fs } from 'fs';
+import path from 'path';
+import { IDiscordCommand } from '../interfaces/discordCommand.interface';
+
+const allowedExtractors = ['SpotifyExtractor', 'SoundCloudExtractor'];
+
+export class DiscordClient {
+  commands?: Collection<string, IDiscordCommand>;
+  constructor(
+    private readonly logger: ILogger,
+    private readonly client: Client
+  ) {}
+
+  async turnsOn() {
+    this.client.once(Events.ClientReady, (readyClient) => {
+      this.logger.logInfo(`I'm ready as: ${readyClient.user.tag}`);
+    });
+    await this.loadCommands();
+    await this.loadInteractionResolver();
+    await this.client.login(envs.API_TOKEN);
+  }
+
+  async addPlayer() {
+    const player = new Player(this.client);
+    await player.extractors.loadDefault((extractor) =>
+      allowedExtractors.includes(extractor)
+    );
+    player.events.on(PlayerEvent.Error, (_, error) => {
+      this.logger.logInfo(`Player error event: ${error.message}`);
+    });
+  }
+
+  async loadCommands() {
+    this.commands = new Collection();
+    const commandsPath = path.resolve('src', 'commands');
+    const commandsFilesPaths = await fs.readdir(commandsPath);
+    const commands = commandsFilesPaths.map((file) =>
+      import(`${commandsPath}/${file}`).then<IDiscordCommand>(
+        (command) => command.default
+      )
+    );
+    for await (const command of commands) {
+      this.commands.set(command.data.name, command);
+    }
+  }
+
+  async loadInteractionResolver() {
+    this.client.on(
+      Events.InteractionCreate,
+      async (interaction: Interaction) => {
+        if (!interaction.isChatInputCommand()) return;
+        const command = this.findCommand(interaction);
+        if (!command) return;
+        await this.executeCommand(command, interaction);
+      }
+    );
+  }
+
+  findCommand(interaction: CommandInteraction): IDiscordCommand | undefined {
+    return this.commands!.get(interaction.commandName);
+  }
+
+  async executeCommand(
+    command: IDiscordCommand,
+    interaction: CommandInteraction
+  ) {
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      this.logger.logError((error as Error).message);
+    }
+  }
+}
